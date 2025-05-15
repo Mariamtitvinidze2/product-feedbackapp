45a01d1e55dc6f708dc175674f03f7431aafaac8
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-05-15 20:31:06 +0400

    feat(RoadMapRouter):add_roadmaprouter

--- src/app/common/components/__molecules/RoadMapHeader/RoadMapHeader.tsx
+++ src/app/common/components/__molecules/RoadMapHeader/RoadMapHeader.tsx
@@ -1,15 +1,20 @@
+'use client'
 import LeftArrow from "@/app/common/icons/LeftArrow";
 import AddFeedBackBtn from "../../__atoms/addFeedBackBtn/AddFeedBackBtn";
-
+import { useRouter } from "next/navigation";
 export default function RoadMapHeader() {
+    const router = useRouter()
+    const GoBack = () => {
+        router.push('inside')
+    }
     return <>
         <div className="w-full flex px-8 py-[27px] bg-indigoink rounded-[10px] items-center justify-between ">
             <div className="">
-                <div className="flex gap-3 items-center "><LeftArrow /><h2 className="text-white font-bold text-[14px]">Go Back</h2></div>
+                <button onClick={GoBack} className="flex gap-3 items-center "><LeftArrow /><h2 className="text-white font-bold hover:underline  text-[14px]">Go Back</h2></button>
                 <h1 className="text-white font-bold text-[24px]">Roadmap</h1>
             </div>
             <AddFeedBackBtn />
         </div>
-        
+
     </>
 }
