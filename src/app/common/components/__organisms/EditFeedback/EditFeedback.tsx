41f79cf131eec3088c9f6715c71b508d625b1a8b
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-05-17 22:58:19 +0400

    vcadot

---
+++ src/app/common/components/__organisms/EditFeedback/EditFeedback.tsx
@@ -0,0 +1,13 @@
+import BigPlusSvg from "@/app/common/icons/BigPlusSvg";
+
+export default function EditFeedback() {
+    return <>
+        <div className="w-full px-[42px] pt-[52px] rounded-[10px] flex flex-col gap-[75px] pb-10 bg-white relative max-[550px]:gap-6 max-[550px]:p-6 max-[550px]:pt-11">
+            <div className="absolute w-[56px] h-[56px] flex justify-center items-center bg-red-600 rounded-full -top-7 max-[550px]:w-10 max-[550px]:h-10 max-[550px]:-top-5 "><BigPlusSvg /></div>
+            <div className="">
+                <h1 className="text-[#3A4374] text-[24px] font-bold max-[550px]:text-[18px]">Editing ‘Add a dark theme option’</h1>
+            </div>
+
+        </div>
+    </>
+}
