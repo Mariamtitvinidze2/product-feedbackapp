221cb84d1f82dc04f0da68b990ea22d6e5e6cea2
mariam titvinidze <titvinidzemariam3@gmail.com>
2025-05-18 13:29:13 +0400

    .

--- src/app/common/components/__molecules/RoadMapHeader/RoadMapHeader.tsx
+++ src/app/common/components/__molecules/RoadMapHeader/RoadMapHeader.tsx
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 import AddFeedBackBtn from "../../__atoms/addFeedBackBtn/AddFeedBackBtn";
 import GoBackBtn from "../../__atoms/goBackBtn/goBackBtn";
 
 export default function RoadMapHeader() {
-
-    return <>
-        <div className="w-full flex px-8 py-[27px] bg-indigoink rounded-[10px] items-center justify-between ">
-            <div className="">
-                <GoBackBtn className='#ffffff'/>
-                <h1 className="text-[#ffffff] font-bold text-[24px]">Roadmap</h1>
-            </div>
-            <AddFeedBackBtn />
+  return (
+    <>
+      <div className="w-full flex px-8 py-[27px] bg-indigoink rounded-[10px] items-center justify-between ">
+        <div className="">
+          <GoBackBtn className="#ffffff" />
+          <h1 className="text-[#ffffff] font-bold text-[24px]">Roadmap</h1>
         </div>
-
+        <AddFeedBackBtn />
+      </div>
     </>
+  );
 }
