698d5168f62af2f4b03f62b465407c47d7cf6dd5
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-05-18 14:25:03 +0400

    feat(editfeedback):add_responsive
    mtliani responsivi davamate

--- src/app/(pages)/editfeedback/page.tsx
+++ src/app/(pages)/editfeedback/page.tsx
@@ -4,7 +4,7 @@ import EditFeedback from "@/app/common/components/__molecules/feedbackEdit/EditF
 
 export default function FeedbackEditPage() {
     return <>
-        <div className="w-full bg-[#F7F8FD] pb-[129px] px-6   pt-[92px]  max-[550px]:pt-[34px]">
+        <div className="w-full bg-[#F7F8FD] pb-[129px] px-6  max-[800px]:pt-[52px] pt-[92px]  max-[550px]:pt-[34px] max-[500px]:pb-[77px]">
             <div className="w-full max-w-[540px] mx-auto flex flex-col gap-[68px] max-[550px]:max-w-[327px] transition-all duration-300">
                 <GoBackBtn className="#647196" />
                 <EditFeedback />
